b6774fa409eda4953bbf80fff835ed4f382ace52
guemin96 <guemin12@naver.copm>
2021-04-09 17:14:03 +0900

    210409 커밋

---
+++ DevHelloAspNet/FirstWebApp/FrmResponseWrite.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmResponseWrite.aspx.cs" Inherits="FirstWebApp.FrmResponseWrite" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="BtnMessage" runat="server" 
+                Text="클릭" OnClick="BtnMessage_Click" />
+            <br />
+            <%="또 만나요 <br />" %>
+            <asp:Button ID="BtnJs" runat="server"
+                Text="JS Greeting" OnClick="BtnJs_Click" />
+            <br />
+            <asp:LinkButton ID="BtnLink" runat="server"
+                text="네이버" OnClick="BtnLink_Click"> 네이버 </asp:LinkButton>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DevHelloAspNet/FirstWebApp/FrmResponseWrite.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstWebApp
+{
+    public partial class FrmResponseWrite : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("안녕하세요.<br />");
+        }
+
+        protected void BtnMessage_Click(object sender, EventArgs e)
+        {
+            Response.Write("<p style='color:blue;'>반갑습니다.</p>");
+            Response.Write("<span style='color:blue;'>p와 span의 차이를 알겠지?.</span>");
+            //span은 딱 붙어서 나오는 반면 p는 paragrape라는 말처럼 위아래로 한칸씩 띄어져 나옴
+        }
+
+        protected void BtnJs_Click(object sender, EventArgs e)
+        {
+            string strScript = @"<script language = 'javascript'>
+                                  window.alert('안녕하세요!');
+                                 </script>";
+            Response.Write(strScript);
+                                    
+        }
+
+        protected void BtnLink_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("http://www.naver.com");
+        }
+    }
+}
