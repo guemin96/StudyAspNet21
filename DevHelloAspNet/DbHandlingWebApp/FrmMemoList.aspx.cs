ce88d978dc60d10390264ee3fcce9da5230b4377
guemin96 <guemin12@naver.copm>
2021-04-13 17:02:35 +0900

    커밋

---
+++ DevHelloAspNet/DbHandlingWebApp/FrmMemoList.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DbHandlingWebApp
+{
+    public partial class FrmMemoList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+
+            using (var conn = new SqlConnection(connString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+
+                SqlCommand cmd = new SqlCommand("ListMemo", conn); // DB테이블(저장프로시저)에 ListMemo가 있음
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Memos");
+
+                GrvMemoList.DataSource = ds;
+                GrvMemoList.DataBind();
+            }
+        }
+
+        protected void BtnSearch_Click(object sender, EventArgs e)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+
+            using (var conn = new SqlConnection(connString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SearchMemo", conn); // DB테이블(저장프로시저)에 SearchMemo가 있음
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@SearchField", CboSearch.SelectedValue); // Name, Title
+                cmd.Parameters.AddWithValue("@SearchQuery", TxtSearch.Text.Replace("--",""));
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Memos");
+
+                GrvMemoList.DataSource = ds.Tables[0].DefaultView;
+                GrvMemoList.DataBind();
+            }
+
+        }
+    }
+}
---
+++ DevHelloAspNet/DbHandlingWebApp/Models/MaximRepository.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DbHandlingWebApp.Models
+{
+    public class MaximRepository
+    {
+        private IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["connString"].ConnectionString);
+        
+        //입력
+        public Maxims AddMaxims(Maxims maxims)
+        {
+            string query = @"INSERT INTO Maxims (Name, Contents, RegDate) Values (@Name,@Contents,getdate());
+                             SELECT CASE(SCOPE_IDENTITY() AS INT);";
+            var id = db.Query<int>(query, maxims).Single();
+            maxims.Id = id;
+            return maxims;
+        }
+        //조회
+
+        //수정
+
+        //삭제
+    }
+}
